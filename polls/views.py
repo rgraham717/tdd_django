d3b5071c688c754c4f3d824e52530c1aec8f1ff4
Rob Graham <rgraham717@gmail.com>
2013-02-12 06:08:01 -0500

    midway through page 4

---
+++ polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from polls.models import Poll
+
+def home(request):
+    context = {'polls': Poll.objects.all()}
+    return render(request, 'home.html', context)
+
+def poll(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    return render(request, 'poll.html', {'poll' : poll})
